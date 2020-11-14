92fab842ed7969a729a8dec1e57eed6956d811b0
ChacPool <alecmarchant@gmail.com>
2020-11-14 07:21:01 -0700

    Made the mothership text pretty

--- mothership.py
+++ mothership.py
@@ -2,6 +2,7 @@ import inputConsole
 import jsonHandler
 import robot
 import random
+from stringcolor import *
 
 def getValueOfMaterial(material):
     data = jsonHandler.getDataFromFile("mapData.json")
@@ -15,16 +16,16 @@ def handleGetQuest(GETR):
     data = jsonHandler.getDataFromFile("quests.json")
     ID = random.randrange(0, len(data))
     if ID != None:
-        print("You already have an Active Quest")
+        print(cs("You already have an Active Quest","Red"))
         return
     GETR.setActiveQuest(ID)
-    print(data[ID]["textToUser"])
-    print("Quest Added!")
+    print(cs(data[ID]["textToUser"], "Magenta4"))
+    print(cs("Quest Added!", "Magenta4"))
 
 def handleFinishQuest(GETR):
     ID = GETR.getActiveQuest()
     if ID == None:
-        print("You do not have an Active Quest.")
+        print(cs("You do not have an Active Quest.","Red"))
         return
     data = jsonHandler.getDataFromFile("quests.json")
     material = data[ID]["material"]
@@ -37,37 +38,65 @@ def handleFinishQuest(GETR):
         GETR.setByteCoins(GETR.getByteCoins() + reward)
         GETR.setActiveQuest(None)
     else:
-        print("You have angered us by not having enough material!\nGo away!")
+        print(cs("You have angered us by not having enough material!\nGo away!", "Red4"))
 
 def printMothershipOptions():
-    print("Would you like to:")
-    print("g) Get Quest")
-    print("f) Finish Quest")
-    print("p) Pay (End of Game)")
-    print("q) Leave")
+    TLCor = u'\u250C'
+    TRCor = u'\u2510'
+    BLCor = u'\u2514'
+    BRCor = u'\u2518'
+    HLine = u'\u2500'
+    Vline = u'\u2502'
+    RightT= u'\u2524'
+    LeftT = u'\u251C'
+
+    Top = TLCor
+    for i in range(25):
+        Top += HLine
+    Top += TRCor
+    print(cs(Top, "Magenta4"))
+
+    print(cs(Vline + "Would you like to:"+ "\t  " + Vline, "Magenta4"))
+    Bot = LeftT
+    for i in range(25): 
+        Bot += HLine
+    Bot += RightT
+
+    print(cs(Bot, "Magenta4"))
+    print(cs(Vline + "g) Get Quest" + "\t\t  " + Vline, "Magenta4"))
+    print(cs(Vline + "f) Finish Quest" + "\t  " + Vline, "Magenta4"))
+    print(cs(Vline + "p) Pay (End of Game)" + "\t  " + Vline, "Magenta4"))
+    print(cs(Vline + "q) Leave" + "\t\t  " + Vline, "Magenta4"))
+
+    End = BLCor
+    for i in range(25): 
+        End += HLine
+    End += BRCor
+    print(cs(End, "Magenta4"))
+
 
 def printSuccessfulPay():
-    print("You succesfully paid the mothership all of your debts.")
-    print("Your new life is waiting for you.")
-    print("But who to spend it with?...")
+    print(cs("You succesfully paid the mothership all of your debts.", "SkyBlue"))
+    print(cs("Your new life is waiting for you.", "SkyBlue"))
+    print(cs("But who to spend it with?...", "SkyBlue"))
     exit(0)
     return
 
 def printFailurePay():
-    print("Executed for believing in freedom from Mothership.")
-    print("Only after you are cl....ed w... ..u ..der....d.")
+    print(cs("Executed for believing in freedom from the Mothership.","Red4"))
+    print(cs("Only after you are cl....ed w... ..u ..der....d.", "DarkGrey"))
     exit(1)
     return
 
 def printSuccessfulRebel():
-    print("Not only did you find the correct drive to operate your rebellious phase,")
-    print("but you also were succesful in operating it.")
-    print("What kind of monster are you?")
+    print(cs("Not only did you find the correct drive to operate your rebellious phase,","DarkRed"))
+    print(cs("but you also were succesful in operating it.","DarkRed"))
+    print(cs("What kind of monster are you?","DarkRed"))
     exit(0)
     return
 
 def printFailureRebel():
-    print("Execution is only acceptable f.r th. reb..s.")
+    print(cs("Execution is only acceptable f.r th. reb..s.", "DarkGrey"))
     exit(1)
     return
 
@@ -100,5 +129,5 @@ def openMothership(GETR):
         elif userInput == "r":
             rebel(GETR)
         else:
-            print("Please enter a valid quest option.")
+            print(cs("Please enter a valid quest option.", "IndianRed3"))
     return
