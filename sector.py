c4327f2ec3d98fecda689bbd6d0eb8bebc5d1390
ChacPool <alecmarchant@gmail.com>
2020-11-13 19:01:00 -0700

    Here be my stuff

--- sector.py
+++ sector.py
@@ -1,8 +1,21 @@
 import gridMap
+import jsonHandler
+import random
 
 class Sector(gridMap.GridMap):
-    def __init__(self, width, height):
-        gridMap.GridMap.__init__(self, width, height)
+    def __init__(self, width, height, planet):
+        gridMap.GridMap.__init__(self, 11, 11)
+        self.planet = planet
+        self.map[6][6] = 1
+        self.generateMap(self.planet)
 
-    def generateMap(self):
-        return
+
+    def generateMap(self, planet):
+        data = jsonHandler.getDataFromFile("planetData.json")
+        concentrations = data["locationList"][planet][concentration]
+        for i in range(self.width):
+            for j in range(self.height):
+                for key in concentrations:
+                    if random.randrange(0, 100) <= concentrations[key]:
+                        self.map[i][j] = key
+                        break
